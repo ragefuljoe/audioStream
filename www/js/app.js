2a8ea25304bf6a737871be97470f4b34c236abac
matthew yang <matt@matthewcyang.com>
2014-08-30 11:33:15 -0500

    set home to be default route

--- www/js/app.js
+++ www/js/app.js
@@ -88,6 +88,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
 
